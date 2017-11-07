90bbe8884166729d4f5396772d064775127326fb
fede_zurbriggen <fedezurbriggen@gmail.com>
2017-11-07 19:16:51 -0300

    fede

---
+++ RafaelaDenunciaWS/RafaelaDenunciaWS/Areas/HelpPage/ModelDescriptions/IModelDocumentationProvider.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Reflection;
+
+namespace RafaelaDenunciaWS.Areas.HelpPage.ModelDescriptions
+{
+    public interface IModelDocumentationProvider
+    {
+        string GetDocumentation(MemberInfo member);
+
+        string GetDocumentation(Type type);
+    }
+}
---
+++ RafaelaDenunciaWS/RafaelaDenunciaWS/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace RafaelaDenunciaWS.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
